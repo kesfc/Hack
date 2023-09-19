c9dec55897e0ac25fcef7e60adc6404cd8f1fcb8
kesfc <kevinli20030305@gmail.com>
2023-09-19 17:07:13 -0400

    didn't really do anything
    didn't really do anything

---
+++ frontend/src/Prize/prize.js
@@ -0,0 +1,15 @@
+// Import the necessary React libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Define a functional component called App
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+// Render the App component to the root element in the HTML
+ReactDOM.render(<App />, document.getElementById('root'));
